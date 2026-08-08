6e036a0bb2cd7220e4afb322a334225b751d52ec
KariMahfoudi <kimomhf@gmail.com>
2026-08-08 11:48:26 +0100

    SettingsForNotif

--- extension.js
+++ extension.js
@@ -1,41 +1,58 @@
 const Main = imports.ui.main;
+const Settings = imports.ui.settings;
 
 class MintSnapExtension {
     constructor() {
         this.enabled = false;
+        this.settings = null;
+        this.focusSignal= null;
     }
 
     enable() {
         this.enabled = true;
 
+        this.settings = new Settings.ExtensionSettings(this, "mintsnap@krim");
+        this.settings.bind("Notify focus changes", "notifyFocusChanges", this._onNotifyFocusChangesChanged.bind(this));
         global.log("[MintSnap] enabled");
 
         Main.notify(
             "MintSnap",
             "Extension successfully loaded"
         );
-    
-        let window = global.display.focus_window;
-    
-        if (window){
-            global.log("[MintSnap] Active window:" + window.get_title());
 
+        this.focusSignal = global.display.connect('notify::focus-window', ()=>{this.onFocusChanged});
+    }
+
+    onFocusChanged(){
+        let window = global.display.focus_window;
+        if(!window){
+            return;
+        }
+
+        let title = window.get_title();
+        global.log("[MintSnap] Focus changed to: " + title);
+        if (this.notifyFocusChange){
             Main.notify(
                 "MintSnap",
-                "Active window: " + window.get_title()
-            );
-
+                "Active window:" +title);
         }
-    else {
-        Main.notify(
-            "MintSnap",
-            "No active window"
-        );
-    }    
+
     }
+
+    
+    
+        l
 
     disable() {
         this.enabled = false;
+ 
+        if (this.focusSignal !== null){
+            global.display.disconnect(this.focusSignal);
+            this.focusSignal = null;
+        }
+
+
+
         global.log("[MintSnap] disabled");
     }
 }
